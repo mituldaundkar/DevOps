4bb616b2f9fca1a5131d14f99d116d4260cc67ce
Mitul <mituldaundkar@gmail.com>
2026-01-04 20:10:10 +0530

    day 2

--- Day 2/main.tf
+++ Day 2/main.tf
@@ -21,4 +21,18 @@ resource "aws_security_group" "my-sg" {
     }
     vpc_id = "vpc-0a9802b16c47deb59"
 
+    ingress{
+        protocol = "tcp"
+        from_port = 80
+        to_port = 80
+        cidr_blocks = "0.0.0.0/0"
+    }
+
+     egress{
+        protocol = "-1"
+        from_port = 0
+        to_port = 0
+        cidr_blocks = "0.0.0.0/0"
+    }
+
 }
