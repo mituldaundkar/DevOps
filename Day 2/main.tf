10e1d87b96f31e7fabbad0d12dbd155adf22bd59
Mitul <mituldaundkar@gmail.com>
2026-01-04 20:16:07 +0530

    day 2

--- Day 2/main.tf
+++ Day 2/main.tf
@@ -25,14 +25,14 @@ resource "aws_security_group" "my-sg" {
         protocol = "tcp"
         from_port = 80
         to_port = 80
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
      egress{
         protocol = "-1"
         from_port = 0
         to_port = 0
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
 }
